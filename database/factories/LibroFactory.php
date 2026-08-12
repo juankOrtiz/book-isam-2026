576280203badd39b992de076d65fb18fd6bf7d40
juankOrtiz <juankortiz@proton.me>
2026-08-12 14:09:06 -0300

    Crear modelo, factory y seeder de Libro

---
+++ database/factories/LibroFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Model>
+ */
+class LibroFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => fake()->sentence(3),
+            'resumen' => fake()->paragraph(3),
+            'autor' => fake()->name(),
+            'puntaje' => fake()->numberBetween(1, 5),
+        ];
+    }
+}
